46d22c56dd0239f2e6ec90c1bb07ebdfad28c2e0
Nikhil Aji <nklajiofficial@gmail.com>
2026-05-05 18:47:37 -0400

    Finished text and query embedding

---
+++ app/services/embedder.py
@@ -0,0 +1,46 @@
+from config import EMBEDDING_MODEL
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer(EMBEDDING_MODEL)
+
+
+def embd_text(text):
+    """
+    Embed the text chunks extracted from documents
+    before storing it into ChromaDB
+
+    Args:
+    text: text chunks to embed.
+
+    Returns:
+    list of vector embeddings
+
+    Raises:
+    Runtime error: if the embedding model fails to encode the texts
+    """
+    try:
+        embeddings = model.encode(text, normalize_embeddings=True)
+        return embeddings.tolist()
+    except Exception as e:
+        raise RuntimeError(f"Error encoding text: {e}")
+
+
+def embd_query(query):
+    """
+    Embed the query text into vector for similarity search
+    in ChromaDB
+
+    Args:
+    query: The users question
+
+    Returns:
+    embedding vector as a list
+
+    Raises:
+    Runtime error: if the embedding model fails to encode the query
+    """
+    try:
+        embedding = model.encode([query], normalize_embeddings=True)
+        return embedding[0].tolist()
+    except Exception as e:
+        raise RuntimeError(f"Error encoding query: {e}")
