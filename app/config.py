2978dc2aa2fa37c39790f47028feb045ba60fe24
Nikhil Aji <nklajiofficial@gmail.com>
2026-05-08 13:51:32 -0400

    added .env variable and model name

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+
+load_dotenv()
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
 CHROMA_DB_PATH = "/data/chroma_db"
 CHROMA_RESULTS_RETURNED = 5
+MODEL_NAME = "gemini-3-flash-preview"
