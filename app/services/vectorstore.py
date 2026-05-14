9a8077e17fe8b12f64cd992c5834b659c5500195
Nikhil Aji <nklajiofficial@gmail.com>
2026-05-14 10:14:13 -0400

    Added return for add_chunks() function

--- app/services/vectorstore.py
+++ app/services/vectorstore.py
@@ -1,7 +1,8 @@
 import uuid
 
 import chromadb
-from config import CHROMA_DB_PATH, CHROMA_RESULTS_RETURNED
+
+from app.config import CHROMA_DB_PATH, CHROMA_RESULTS_RETURNED
 
 chroma_client = chromadb.PersistentClient(path=CHROMA_DB_PATH)
 
@@ -18,7 +19,7 @@ def add_chunks(chunks, embeddings, filename):
         filename: from which file those chunks are from
 
     Returns:
-            None
+            number of chunks inserted
 
     Raises:
         RuntimeError: if the chunks were failed to insert in ChromaDB
@@ -34,6 +35,8 @@ def add_chunks(chunks, embeddings, filename):
         )
     except Exception as e:
         raise RuntimeError(f"Error while adding documents to ChromaDB: {e}")
+
+    return len(chunks)
 
 
 def search(query_vector):
@@ -57,4 +60,40 @@ def search(query_vector):
         )
         return results
     except Exception as e:
-        raise RuntimeError(f"Error while shearching from ChromaDB: {e}")
+        raise RuntimeError(f"Error while searching from ChromaDB: {e}")
+
+
+def delete_by_file_name(filename):
+    """_summary_
+
+    Args:
+        filename (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    try:
+        results = collection.get(where={"filename": filename})
+        ids = results["ids"]
+        if ids:
+            collection.delete(ids=ids)
+            return len(ids)
+        return 0
+    except Exception as e:
+        raise RuntimeError(f"Error deleting the file: {e}")
+
+
+def get_stats():
+    try:
+        total = collection.count()
+
+        if total == 0:
+            return {"total_chunks": 0, "documents": []}
+
+        all_metadata = collection.get(include=["metadatas"])["metadatas"]
+        documents = sorted(set(m["filename"] for m in all_metadata))
+
+        return {"total_chunks": total, "documents": documents}
+
+    except Exception as e:
+        raise RuntimeError(f"Error while retreving stats from ChromaDB: {e}")
