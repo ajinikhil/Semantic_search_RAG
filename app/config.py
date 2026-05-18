b9b288ce412a2b922387a1adcccb6806d4207f0a
Nikhil Aji <nklajiofficial@gmail.com>
2026-05-18 19:56:05 -0400

    Organized config. closed #4
    next steps. write main.py and test the endpoints with uvicorn and then with postman.
    
    after that test it with Pytest and then start doing the front end.
    
    thats it :)

--- app/config.py
+++ app/config.py
@@ -1,12 +1,28 @@
 from dotenv import load_dotenv
 
+# This is to load environment variables from .env
+
 load_dotenv()
+
+# Embedding Model Configuration
+
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
+# Text Chunking Configuration
+
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
+
+# ChromaDB Configuration
 CHROMA_DB_PATH = "data/chroma_db"
 CHROMA_RESULTS_RETURNED = 5
+TOP_K = 5
+
+# LLM Configuration
+
 MODEL_NAME = "gemini-3-flash-preview"
+
+# Configuration For File Uploads
+
 ALLOWED_EXTENSIONS = {".pdf", ".docx", ".txt"}
 UPLOAD_DIR = "uploads"
-TOP_K = 5
